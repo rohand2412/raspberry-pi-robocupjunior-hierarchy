55b8e901c56ad8f4e0101dba05fedb902a9133c1
Rohan Dugad <rohand2468@gmail.com>
2021-05-29 23:15:10 -0400

    Finished rpi

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import subprocess
 
 def main():
     """Main code"""
-    while True:
-        if not check_for_reset("python3 spammer-1.py"):
-            if not check_for_reset("python3 spammer-2.py"):
-                break
+    for i in range(3):
+        while True:
+            if not check_for_reset("python3 /home/pi/Documents/PyFiles/raspberry-pi-robocupjunior-line/main.py --mode silver"):
+                if not check_for_reset("python3 /home/pi/Documents/PyFiles/raspberry-pi-robocupjunior-evac/main.py"):
+                    if not check_for_reset("python3 /home/pi/Documents/PyFiles/raspberry-pi-robocupjunior-line/main.py --mode red"):
+                        break
 
 def check_for_reset(command):
     """Checks printout of command for reset flag"""
